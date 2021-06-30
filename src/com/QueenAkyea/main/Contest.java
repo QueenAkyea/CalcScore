37ec66e37663c132731ec5e19f7581c53c55d134
QueenAkyea <queenakyea@gmail.com>
2021-06-30 01:42:16 -0700

    implemented addContestant
    Signed-off-by: Akyea <QueenAkyea@gmail.com>

--- src/com/QueenAkyea/main/Contest.java
+++ src/com/QueenAkyea/main/Contest.java
@@ -2,15 +2,18 @@ package com.QueenAkyea.main;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Contest {
     private ArrayList<Contestant> contestants;
     private int rounds;
-    //private int numContestants;
+    private int numContestants;
 
     public Contest(ArrayList<Contestant> contestants) {
         this.contestants = contestants;
         rounds = 0;
+        numContestants = contestants.size();
     }
 
     public Contest() {
@@ -24,18 +27,103 @@ public class Contest {
     //contestants is always sorted
     public void addContestant(Contestant c) {
         contestants.add(c);
+        numContestants++;
         Collections.sort(contestants);
     }
 
     public void createContestant(String name, double score, int numSiblings, double bodyTemp) {
         Contestant c = new Contestant(name, score, numSiblings, bodyTemp);
-        addContestant(c);
+        this.addContestant(c);
+    }
+
+    public void createContestant() {
+        Scanner sc = new Scanner(System.in);
+        boolean gun = true;
+        double score = 0.0;
+        int numSiblings = 0;
+        double bodyTemp = 0.0;
+
+        while (gun) {
+            System.out.println("> What is their name?");
+            String name = sc.next();
+
+            //contestant's starting score
+            boolean validInput = false;
+            while (!validInput) {
+                try {
+                    System.out.println("> Would you like to give them a starting score? (y/n)");
+                    String input = sc.nextLine();
+                    if (input.equals("y")) {
+                        try {
+                            System.out.println("> What would you like their score to be?");
+                            score = sc.nextDouble();
+                        } catch (InputMismatchException a) {
+                            System.out.println(ConsoleColors.RED_BOLD+ "⚠ NOT A VALID SCORE >:( ⚠" + ConsoleColors.RESET);
+                            System.out.println(ConsoleColors.BLUE + "*doubles must be in decimal format" + ConsoleColors.RESET);
+                        }
+                    } else if (input.equals(("n"))){
+                        score = 0;
+                    } else {
+                        throw new InputMismatchException();
+                    }
+                    validInput = true;
+                } catch (InputMismatchException a) {
+                    System.out.println(ConsoleColors.RED_BOLD+ "⚠ NOT A VALID RESPONSE >:( ⚠" + ConsoleColors.RESET);
+                }
+            }
+            //contestant's siblings
+            boolean validNum = false;
+            while (!validNum) {
+                try {
+                    System.out.println("> How many siblings do they have?");
+                    numSiblings = sc.nextInt();
+                    validNum = true;
+                } catch (InputMismatchException a) {
+                    System.out.println(ConsoleColors.RED_BOLD+ "⚠ NOT A VALID NUMBER >:( ⚠" + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.BLUE + "*this number cannot have a decimal" + ConsoleColors.RESET);
+                }
+            }
+            //contestant's body temperature
+            boolean validTemp = false;
+            while (!validTemp) {
+                try {
+                    System.out.println("> What is their body temperature?");
+                    bodyTemp = sc.nextDouble();
+                    validTemp = true;
+                } catch (InputMismatchException a) {
+                    System.out.println(ConsoleColors.RED_BOLD+ "⚠ NOT A VALID TEMPERATURE >:( ⚠" + ConsoleColors.RESET);
+                    System.out.println(ConsoleColors.BLUE + "*doubles must be in decimal format" + ConsoleColors.RESET);
+                }
+            }
+            //adds new contestant to the contest
+            this.createContestant(name, score, numSiblings, bodyTemp);
+            System.out.println(ConsoleColors.BLUE + "Added contestant \"" + name + "\"" + ConsoleColors.RESET);
+
+            //prompts user if they want to add another contestant
+            validInput = false;
+            while (!validInput) {
+                try {
+                    System.out.println("> Would you like to add another contestant? (y/n)");
+                    String input = sc.nextLine();
+                    if (input.equals("y")) {
+
+                    } else if (input.equals("n")) {
+                        gun = false;
+                    } else {
+                        throw new InputMismatchException();
+                    }
+                    validInput = true;
+                } catch (InputMismatchException a) {
+                    System.out.println(ConsoleColors.RED_BOLD+ "⚠ NOT A VALID RESPONSE >:( ⚠" + ConsoleColors.RESET);
+                }
+            }
+        }
     }
 
     public void endRound() {
         rounds++;
 
-    }
+    } //TODO: implement
 
     /* TODO:
      *   implement addContestant *
